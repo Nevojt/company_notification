e92dae108151ffc3aed2ddabecff794bded4e8a0
Nevojt <nevojt@gmail.com>
2024-06-12 20:39:22 +0200

    encoded

--- app/routers/func_notification.py
+++ app/routers/func_notification.py
@@ -1,20 +1,55 @@
 
 from http.client import HTTPException
+from typing import Optional
 import logging
 from app import models
+from app.config import settings
 from sqlalchemy.future import select
 from sqlalchemy import update
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.orm import joinedload
 
 from app.schemas import InvitationSchema
-
+import base64
+from cryptography.fernet import Fernet, InvalidToken
 
 # Configure logging
 logging.basicConfig(filename='_log/func_notification.log', format='%(asctime)s - %(levelname)s - %(message)s')
 logger = logging.getLogger(__name__)
 
 
+
+# Ініціалізація шифрувальника
+key = settings.key_crypto
+cipher = Fernet(key)
+
+def is_base64(s):
+    try:
+        return base64.b64encode(base64.b64decode(s)).decode('utf-8') == s
+    except Exception:
+        return False
+
+async def async_encrypt(data: Optional[str]):
+    if data is None:
+        return None
+    
+    encrypted = cipher.encrypt(data.encode())
+    encoded_string = base64.b64encode(encrypted).decode('utf-8')
+    return encoded_string
+
+async def async_decrypt(encoded_data: Optional[str]):
+    if encoded_data is None:
+        return None
+    
+    if not is_base64(encoded_data):
+        return encoded_data
+
+    try:
+        encrypted = base64.b64decode(encoded_data.encode('utf-8'))
+        decrypted = cipher.decrypt(encrypted).decode('utf-8')
+        return decrypted
+    except InvalidToken:
+        return None  
 
 async def check_new_messages(session: AsyncSession, user_id: int):
     """
@@ -38,14 +73,14 @@ async def check_new_messages(session: AsyncSession, user_id: int):
 
         # Extract relevant data for each message
         message_data = [
-    {
-        "sender_id": message.sender_id,
-        "sender": message.user_name,
-        "message_id": message.id,
-        "message": message.message,
-        "fileUrl": message.fileUrl,
-    } for message in messages
-]
+                        {
+                            "sender_id": message.sender_id,
+                            "sender": message.user_name,
+                            "message_id": message.id,
+                            "message": "Message encoded",
+                            "fileUrl": message.fileUrl,
+                        } for message in messages
+                    ]
 
 
         return message_data
