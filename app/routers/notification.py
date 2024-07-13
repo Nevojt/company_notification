7e62215b39ae4a697d40fe1d3f3e477e4cdf5335
Nevojt <nevojt@gmail.com>
2024-07-13 22:16:53 +0200

    sleep 1 seconds

--- app/routers/notification.py
+++ app/routers/notification.py
@@ -51,6 +51,8 @@ async def web_private_notification(
         while True:
             await websocket.receive_text()
             
+            await asyncio.sleep(1)
+            
             password_changed = await check_user_password(session, user.id, True)
             if password_changed_state != password_changed:
                 password_changed_state = password_changed
